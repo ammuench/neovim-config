8e75a94f7aa6a342fdb4483720b7746565da2fde
Alex Muench <ammuench@gmail.com>
2026-04-10 23:12:16 -0500

    🐛 Fix vue/volar/vue_ls not working right

--- lua/lsps/lang-typescript.lua
+++ lua/lsps/lang-typescript.lua
@@ -3,11 +3,32 @@ local typescript_module = {}
 typescript_module.setup = function(lsp_utils)
   local capabilities = lsp_utils.capabilities()
 
+  -- vue_ls requires vtsls to be running with the @vue/typescript-plugin
+  -- registered as a global plugin so it can forward TS requests.
+  -- See: https://github.com/vuejs/language-tools/wiki/Neovim
+  local vue_language_server_path = vim.fn.stdpath("data")
+    .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+
   -- vtsls
   vim.lsp.config("vtsls", {
     on_attach = lsp_utils.on_attach,
     capabilities = capabilities,
+    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
     root_markers = { "package.json", "tsconfig.json", "jsconfig.json" },
+    settings = {
+      vtsls = {
+        tsserver = {
+          globalPlugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = vue_language_server_path,
+              languages = { "vue" },
+              configNamespace = "typescript",
+            },
+          },
+        },
+      },
+    },
   })
 
   -- denols
@@ -71,13 +92,38 @@ typescript_module.setup = function(lsp_utils)
     },
   })
 
-  -- volar (Vue)
-  vim.lsp.config("volar", {
+  -- vue_ls (Vue)
+  vim.lsp.config("vue_ls", {
+    cmd = { "vue-language-server", "--stdio" },
+    filetypes = { "vue" },
+    root_markers = { "package.json" },
     on_attach = lsp_utils.on_attach,
     capabilities = capabilities,
+    on_init = function(client)
+      client.handlers["tsserver/request"] = function(_, result, context)
+        local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
+        if #clients == 0 then
+          return
+        end
+
+        local ts_client = clients[1]
+        local param = unpack(result)
+        local id, command, payload = unpack(param)
+        ts_client:exec_cmd({
+          title = "vue_request_forward",
+          command = "typescript.tsserverRequest",
+          arguments = { command, payload },
+        }, { bufnr = context.bufnr }, function(_, r)
+          local response = r and r.body
+          local response_data = { { id, response } }
+          ---@diagnostic disable-next-line: param-type-mismatch
+          client:notify("tsserver/response", response_data)
+        end)
+      end
+    end,
   })
 
-  vim.lsp.enable({ "vtsls", "denols", "biome", "oxlint", "eslint", "volar" })
+  vim.lsp.enable({ "vtsls", "denols", "biome", "oxlint", "eslint", "vue_ls" })
 end
 
 return typescript_module
